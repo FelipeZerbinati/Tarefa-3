a6713b5ac2af6b477132a3f5d6e2d7cd68ebc57e
FelipeZerbinati <fezerba@gmail.com>
2025-01-04 12:16:31 -0300

    Tarefa 3 salva

---
+++ Tarefa3.Api/Controllers/NomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tarefa3.Domain.Interfaces.Service;
+using Tarefa3.Domain.Models;
+namespace Tarefa3.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NomeController(IPersonService personService) : ControllerBase
+    {
+        [HttpGet(Name = "GetListPerson")]
+        public async Task<IActionResult> GetAll()
+        {
+            var url = "https://localhost:7025/";
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            return Ok(personService.GetAll());
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Tarefa3.Application/Services/PersonService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Tarefa3.Domain.Interfaces.Service;
+using Tarefa3.Domain.Models;
+
+namespace Tarefa3.Application.Services
+{
+    public class PersonService : IPersonService
+    {
+        public List<Person> GetAll() 
+        {
+            List<Person> list = new List<Person>();
+            var person = new Person()
+            {
+                Id = 0,
+                Name = "Felipe",
+                Cpf = "384.735.928-24"
+
+            };            
+            list.Add(person);
+            var person2 = new Person()
+            {
+                Id = 1,
+                Name = "Leonardo",
+                Cpf = "465.342.678-05"
+
+            };
+            list.Add(person2);
+
+            var person3 = new Person()
+            {
+                Id = 2,
+                Name = "Paulo",
+                Cpf = "456.123.678-29"
+            };
+            list.Add(person3);
+            return list;
+        }
+    }
+}
